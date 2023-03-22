1cfc4fcbf7890c6d5dcbc5cdba91967544f37c28
RIFAF RAHMAN <127125526+MathaThikNai@users.noreply.github.com>
2023-03-22 21:46:37 +0600

    Theatre Square

---
+++ (1A)Theatre_Square.cpp
@@ -0,0 +1,15 @@
+//You don't need an audience to achieve greatness. You need to focus.
+#include<iostream>
+using namespace std;
+int main()
+{
+   long long m, n, a, x, y;
+   cin >> m >> n >> a;
+   if(m % a == 0){x = m / a;}
+   else{x = m / a + 1;}
+   if(n % a == 0){y = n / a;}
+   else{y = n / a + 1;}
+   cout << x * y << endl;
+   return 0;
+ 
+}

46f3e24698c9266348056518d69a24b739fb9049
RIFAF RAHMAN <127125526+MathaThikNai@users.noreply.github.com>
2024-01-31 16:38:29 +0600

    Odd One Out

--- Odd_One_Out(1915A).cpp
+++ Odd_One_Out(1915A).cpp
@@ -1,26 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+ int main()
+ {
+     int t,a,b,c;
 
-int main() 
-{
-  int t;
-  cin>>t;
-  int a, b,c;
-  
-  while(t--)
-  {
-    cin>>a>>b>>c;
-    if(a==b)
-    {
-      cout<<c<<endl;
-    }
-    else if(b==c)
-    {
-      cout<<a<<endl;
-    }
-    else if(c==a)
-    {
-      cout<<b<<endl;
-    }
-  }
-}
+
+     cin>>t;
+
+     while(t--)
+     {
+        cin>>a>>b>>c;
+
+        if(a==b)
+        {
+
+            cout<<c<<endl;
+
+        }
+        else if(b==c)
+        {
+
+            cout<<a<<endl;
+        }
+        else if(c==a)
+        {
+            cout<<b<<endl;
+        }
+     }
+ }
+
